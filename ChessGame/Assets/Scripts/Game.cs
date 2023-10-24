082ab27954acdefae1499f2661a6ce50895630b0
BrylSerhii <iggrooot@gmail.com>
2023-10-24 16:13:31 +0300

    Created game.cs and ChessMan.cs scripts

--- ChessGame/Assets/Scripts/Game.cs
+++ ChessGame/Assets/Scripts/Game.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public GameObject chesspiece;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(chesspiece, new Vector3(0, 0, -1), Quaternion.identity);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
